4f909bbc304ab8f3bb9d292852814e32ccd3b3af
yechan <100present@naver.com>
2025-12-11 20:25:26 +0900

    [FIX]: JwtAuthentication 경로수정, HealthController 추가

---
+++ src/main/java/com/ureka/techpost/domain/auth/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.ureka.techpost.domain.auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping
+    public String health() {
+        return "ok";
+    }
+}
